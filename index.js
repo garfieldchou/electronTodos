5c6bdc40065e4ebc3d7796e88041e326b54a54df
garfield <cmgarfield@gmail.com>
2017-12-26 23:12:01 +0800

    attach a function to click() of 'New Todo' to create separate windows

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {app, BrowserWindow, Menu} = require('electron');
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
@@ -11,11 +12,22 @@ app.on('ready', () => {
   Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow () {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  });
+}
+
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
-      { label: 'New Todo' },
+      {
+        label: 'New Todo',
+        click () { createAddWindow(); }
+      },
       {
         label: 'Quit',
         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
